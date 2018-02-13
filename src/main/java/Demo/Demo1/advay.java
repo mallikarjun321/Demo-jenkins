8d6b50f4ab838f97a9590863d7a8c9f849059ddf
LAPTOP-SUO3NIPV\shreesai <mallikarjun.321@gmail.com>
2018-02-13 19:44:38 +0000

    updated

--- src/main/java/Demo/Demo1/advay.java
+++ src/main/java/Demo/Demo1/advay.java
@@ -7,6 +7,11 @@ public class advay {
 	@Test
 	public void ad() {
 		System.out.println("Mallik");
+		String s= System.getProperty("url");
+		System.out.println(s);
+		System.out.println("advay");
+		//ok
 	}
 
+	
 }
